a141a8e2ef5f677d22b5801952412e93be7393c0
Ben <benedict_sun@dlsu.edu.ph>
2021-08-03 18:13:16 +0800

    Implemented get featured collections

---
+++ functions/utils/collectionQueries.ts
@@ -0,0 +1,35 @@
+/**
+ * For getting all collections.
+ */
+ const GET_ALL_COLLECTIONS: string = `
+ query{
+     allCollections{
+         data{
+             _id
+             name
+             description
+         }
+     }
+ }
+`
+
+/**
+* For getting collection array, either featured or not featured depending on input.
+* To be used in dashboard when showcasing certain collections.
+*/
+const GET_FEATURED_COLLECTIONS: string = `
+ query getFeaturedCollections($featured: Boolean){
+     collections(featured: $featured){
+         data{
+             _id
+             name
+             description
+         }
+     }
+ }
+`
+
+export { 
+    GET_ALL_COLLECTIONS,
+    GET_FEATURED_COLLECTIONS,
+};
